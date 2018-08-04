d1a8c28ff6508b3793587391eb0537084f7491ea
Rajesh Naik <rajesh.naik@apexinfotech.co.uk>
2018-08-04 21:00:48 +0100

    Update moduletwo-start.tf

--- m2/moduletwo-start.tf
+++ m2/moduletwo-start.tf
@@ -7,7 +7,7 @@ variable "aws_secret_key" {}
 variable "private_key_path" {}
 
 variable "key_name" {
-  default = "PluralsightKey"
+  default = "??keyname??"
 }
 
 variable "network_address_space" {
